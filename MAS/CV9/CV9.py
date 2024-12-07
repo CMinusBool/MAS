e8abd49affbec5c6e6a9a6ee2f55d84d66cb2d65
CMinusBool <118272922+CMinusBool@users.noreply.github.com>
2024-12-08 00:40:03 +0100

    CV9 - task 1 probably done

--- MAS/CV9/CV9.py
+++ MAS/CV9/CV9.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import kstest, norm, expon, powerlaw
+from scipy.stats import kstest, norm, expon, powerlaw, poisson, lognorm
 
 # normal distribution
 normal_dist = np.random.normal(0, 10, 5500)
@@ -13,10 +13,6 @@ plt.hist(normal_dist, bins=100, density= True, stacked= True)
 plt.plot(x_axis, norm.pdf(x_axis, normal_fit[0], normal_fit[1]), 'r')
 plt.title('Normal Distribution')
 plt.show()
-
-k_test = kstest(normal_dist, 'norm', args=(normal_fit[0], normal_fit[1]))
-
-print(f"Normal distribution: {k_test}")
 
 # exponential distribution
 exponential_dist = np.random.exponential(1, 5500)
@@ -45,5 +41,46 @@ plt.plot(x_axis, powerlaw.pdf(x_axis, powerlaw_fit[0]), 'r')
 plt.title('Power Law Distribution')
 plt.show()
 
-#k_test = kstest(powerlaw_dist, 'powerlaw')
-print(f"Power Law distribution: {k_test}")
+# Poisson distribution
+poisson_dist = np.random.poisson(lam=8, size=5500)
+poisson_fit = np.mean(poisson_dist)
+x_axis_poisson = np.arange(min(poisson_dist), max(poisson_dist), 1)
+
+# Distribution graph for Poisson
+plt.hist(poisson_dist, bins=20, density=True, label='Histogram')
+plt.plot(x_axis_poisson, poisson.pmf(x_axis_poisson, poisson_fit), 'r', label='Fitted Poisson PMF')
+plt.title('Poisson Distribution')
+plt.show()
+
+# Log-Normal distribution
+lognormal_dist = np.random.lognormal(mean=1, sigma=0.5, size=5500)
+lognormal_fit = lognorm.fit(lognormal_dist)
+x_axis_lognormal = np.linspace(min(lognormal_dist), max(lognormal_dist), 1000)
+
+# Distribution graph for Log-Normal
+plt.hist(lognormal_dist, bins=100, density=True, label='Histogram')
+plt.plot(x_axis_lognormal, lognorm.pdf(x_axis_lognormal, *lognormal_fit), 'r', label='Fitted Log-Normal PDF')
+plt.title('Log-Normal Distribution')
+plt.show()
+
+# Ks test for all distributions
+k_test = kstest(normal_dist, lambda x: norm.cdf(x, normal_fit[0], normal_fit[1]))
+print(f"Ks test for Normal distribution: ")
+print(f"D-value: {k_test[0]:4f}, P-value: {k_test[1]:4f}\n")
+
+k_test = kstest(exponential_dist, lambda x: expon.cdf(x, exponential_fit[0], exponential_fit[1]))
+print(f"Ks test for Exponential distribution: ")
+print(f"D-value: {k_test[0]:4f}, P-value: {k_test[1]:4f}\n")
+
+k_test = kstest(powerlaw_dist, lambda x: powerlaw.cdf(x, powerlaw_fit[0]))
+print(f"Ks test for Power Law distribution:")
+print(f"D-value: {k_test[0]:4f}, P-value: {k_test[1]:4f}\n")
+
+# ks test je pro kontinualni distribuce a ne pro diskrétní
+ks_poisson = kstest(poisson_dist, 'poisson', args=(poisson_fit,))
+print("K-S Test for Poisson Distribution:")
+print(f"D-value: {ks_poisson.statistic:.4f}, P-value: {ks_poisson.pvalue:.4f}\n")
+
+ks_lognormal = kstest(lognormal_dist, lambda x: lognorm.cdf(x, *lognormal_fit))
+print("K-S Test for Log-Normal Distribution:")
+print(f"D-value: {ks_lognormal.statistic:.4f}, P-value: {ks_lognormal.pvalue:.4f}\n")
